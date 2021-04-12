b6bc38a89e6af9aa047b7a2b5f08f153544debd7
Deytalytics-JamesDey <james_dey@hotmail.com>
2021-04-12 10:46:55 +0100

    Add code to create LINK table statements for DV2 Erwin Model

--- erwin_to_json.py
+++ erwin_to_json.py
@@ -8,6 +8,8 @@ cursor = cnxn.cursor()
 out_json = {}
 keys=[]
 
+primary_keys = []
+
 def fetch_model_name():
     sql = """
     SELECT 
@@ -38,6 +40,40 @@ def extract_defs(type_name, sql):
             col_ctr+=1
         rows.append(dict(row_cols))
     return rows
+
+def create_dv2_rels(entity):
+    for subject_area in model["Subject Areas"]:
+        for entity in subject_area["Entities"]:
+            entity_name = entity["Entity Name"].replace(" ","_").upper()
+            sql = """
+            SELECT distinct
+            TRAN(R.Long_Id) "Relationship ID",
+            P.NAME AS 'PARENT',
+            R.PARENT_TO_CHILD_VERB_PHRASE AS 'RELATIONSHIP',
+            C.NAME AS CHILD
+            FROM M0.RELATIONSHIP R INNER JOIN M0.ENTITY P
+            ON R.PARENT_ENTITY_REF = P.ID@
+            INNER JOIN M0.ENTITY C
+            ON R.CHILD_ENTITY_REF = C.ID@
+            WHERE C.HIDE_IN_LOGICAL is null
+            AND P.HIDE_IN_LOGICAL is null
+            AND UPPER(P.NAME) = '%s'
+            ORDER by 2,4
+            """%entity_name
+
+            relationships = extract_defs("Relationships",sql)
+            table_stmt=""
+            for rel in relationships:
+                parent = rel["PARENT"].replace(" ","_").upper()
+                child = rel["CHILD"].replace(" ","_").upper()
+                table_stmt = "CREATE TABLE %s_%s_LINK\n(\n"%(parent,child)
+                table_stmt += "\t%s_%s_HASH CHAR(64) NOT NULL,\n"%(parent,child)
+                table_stmt += "\t%s_HASH CHAR(64) NOT NULL,\n" % (parent)
+                table_stmt += "\t%s_HASH CHAR(64) NOT NULL,\n" % (child)
+            if len(table_stmt) > 0:
+                table_stmt += "\tLOAD_DATE DATE NOT NULL,\n\tSOURCE VARCHAR(100) NOT NULL\n"
+                table_stmt += ")\n"
+                print(table_stmt)
 
 def fetch_model():
     #fetch all of the subject areas
@@ -133,13 +169,14 @@ def create_3nf_model(model):
             print(table_stmt)
 
 def create_dv2_hub(entity):
-    for index, attribute in enumerate(entity["Attributes"]):
-        table_stmt = "CREATE TABLE %s_HUB\n("%entity["Entity Name"].replace(" ","_").upper()
-        for attribute in entity["Attributes"]:
-            if attribute["Primary Key Flag"]=="Primary Key":
-                table_stmt += "\n\t%s %s %s" % (attribute["Attribute Name"].replace(" ","_").upper(), attribute["Attribute Data Type"], attribute["Not Null Flag"].upper())
-        table_stmt += ",\n\tLOAD_DATE DATE NOT NULL,\n\tSOURCE VARCHAR(100) NOT NULL,\n\tEFFECTIVE_FROM DATE NOT NULL"
-        table_stmt += ",\n\t%s_HASH CHAR(64) NOT NULL\n);\n"%(entity["Entity Name"].replace(" ","_").upper())
+    entity_name = entity["Entity Name"].replace(" ","_").upper()
+    table_stmt = "CREATE TABLE %s_HUB\n("%entity_name
+    for attribute in entity["Attributes"]:
+        if attribute["Primary Key Flag"]=="Primary Key":
+            primary_keys.append([entity_name,attribute["Attribute Name"].replace(" ","_").upper(), attribute["Attribute Data Type"], attribute["Not Null Flag"].upper()])
+            table_stmt += "\n\t%s %s %s" % (attribute["Attribute Name"].replace(" ","_").upper(), attribute["Attribute Data Type"], attribute["Not Null Flag"].upper())
+    table_stmt += ",\n\tLOAD_DATE DATE NOT NULL,\n\tSOURCE VARCHAR(100) NOT NULL,\n"
+    table_stmt += "\t%s_HASH CHAR(64) NOT NULL\n);\n"%entity_name
     print(table_stmt)
 
 def create_dv2_sat(entity):
@@ -150,6 +187,7 @@ def create_dv2_sat(entity):
                 table_stmt += "\n\t%s %s %s" % (attribute["Attribute Name"].replace(" ","_").upper(), attribute["Attribute Data Type"], attribute["Not Null Flag"].upper())
         table_stmt += ",\n\tLOAD_DATE DATE NOT NULL,\n\tSOURCE VARCHAR(100) NOT NULL,\n\tEFFECTIVE_FROM DATE NOT NULL"
         table_stmt += ",\n\t%s_HASH CHAR(64) NOT NULL"%(entity["Entity Name"].replace(" ","_").upper())
+        table_stmt += ",\n\t%s_SK IDENTITY NOT NULL"%(entity["Entity Name"].replace(" ","_").upper())
         table_stmt += ",\n\tHASHDIFF CHAR(64) NOT NULL\n);\n"
     print(table_stmt)
 
@@ -158,6 +196,7 @@ def create_dv2_model(model):
         for entity in subject_area["Entities"]:
             create_dv2_hub(entity)
             create_dv2_sat(entity)
+    create_dv2_rels(model)
 
 #create_3nf_model(model)
 
